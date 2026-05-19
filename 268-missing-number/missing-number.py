328fdef88b326cd5c10e642914fbd74e1afc8b85
Kumaradabala729 <165774296+Kumaradabala729@users.noreply.github.com>
2026-05-19 21:44:13 +0530

    Time: 2 ms (57.67%) | Memory: 13.4 MB (36.89%) - LeetSync

---
+++ 268-missing-number/missing-number.py
@@ -0,0 +1,11 @@
+class Solution:
+    
+    def missingNumber(self, nums):
+        
+        n = len(nums)
+        
+        total_sum = n * (n + 1) // 2
+        
+        array_sum = sum(nums)
+        
+        return total_sum - array_sum
