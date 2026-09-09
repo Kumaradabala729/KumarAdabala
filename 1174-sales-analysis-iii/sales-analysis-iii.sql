fd54f28ad4ccf5fd36912e30259c86a006055143
Kumaradabala729 <165774296+Kumaradabala729@users.noreply.github.com>
2026-09-09 23:08:20 +0530

    Time: 1171 ms (78.16%) | Memory: 0B (100.00%) - LeetSync

--- 1174-sales-analysis-iii/sales-analysis-iii.sql
+++ 1174-sales-analysis-iii/sales-analysis-iii.sql
@@ -2,4 +2,4 @@ SELECT DISTINCT p.product_id, p.product_name
 FROM Sales s
 LEFT JOIN Product p ON p.product_id = s.product_id
 GROUP BY p.product_id
-HAVING MIN(sale_date) >= '2019-01-01' AND MAX(sale_date) <= '2019-03-31';
+HAVING MIN(s.sale_date) >= '2019-01-01' AND MAX(s.sale_date) <= '2019-03-31';
